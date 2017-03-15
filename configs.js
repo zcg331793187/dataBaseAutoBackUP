efc11b251d0d09a39b9d523b98e75f3cd846434e
zcg331793187 <zhoucaiguang@gmail.com>
2017-03-15 23:23:55 +0800

    测试脚本

--- configs.js
+++ configs.js
@@ -4,12 +4,10 @@
 "use strict";
 exports.configs = {
     task: "mysql",
-    exec: 'echo node -v;node -v;echo npm -v;npm -v;',
+    exec: 'mysqldump -u root -p147258369 --databases Nodejs >/backup/Nodejs_`date +%F`.sql',
     times: {
         // dayOfWeek:0,//每周的哪一天
         // month:0,//每月
-        // hour:0,//每小时
-        // minute:0,//每分钟
-        second: 1,
+        hour: 6,
     }
 };
--- configs.ts
+++ configs.ts
@@ -20,12 +20,12 @@ interface Iconfigs{
 
 export const configs:Iconfigs ={
     task:"mysql",
-    exec:'echo node -v;node -v;echo npm -v;npm -v;',
+    exec:'mysqldump -u root -p147258369 --databases Nodejs >/backup/Nodejs_`date +%F`.sql',
     times:{
         // dayOfWeek:0,//每周的哪一天
         // month:0,//每月
-        // hour:0,//每小时
+        hour:6,//每小时
         // minute:0,//每分钟
-        second:1,//每分钟的第几秒
+        // second:1,//每分钟的第几秒
     }
 };
